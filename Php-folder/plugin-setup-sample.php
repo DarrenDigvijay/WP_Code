e28d4e631410c322e3f274115888d0ace6eaf7fd
Darren Digvijay <darrendigvijay@darren.digvijay-Latitude-5410>
2022-10-11 16:06:34 +0530

    rescript code smaple

--- Php-folder/plugin-setup-sample.php
+++ Php-folder/plugin-setup-sample.php
@@ -4,8 +4,8 @@
     Plugin URI: sbx-genesis-collection
     Description: The SBX Collection for Genesis Blocks
     Version: 0.1.3
-    Author: Springbox
-    Author URI: https://www.springbox.com
+    Author: Demo
+    Author URI: https://www.demo.com
     License: GPLv3
     License URI: http://www.gnu.org/licenses/gpl-3.0.html
     */
